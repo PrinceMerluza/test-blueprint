b6c8cd72e6820b9faaec978de95896033c6db8e8
Prince Merluza <zero.bash@gmail.com>
2022-03-15 11:36:53 +0800

    Fix vairable names

--- .github/workflows/display-error.sh
+++ .github/workflows/display-error.sh
@@ -4,7 +4,7 @@ count_failed_case=$(jq '.failed|length' result.json)
 count_error_case=$(jq '.error|length' result.json)
 
 counter=0
-while [ $counter -lt $1 ]
+while [ $counter -lt $count_failed_case ]
 do
     failed_case=$(jq .failed[$counter] result.json)
     echo "::error::$failed_case"
@@ -12,7 +12,7 @@ do
 done
 
 counter=0
-while [ $counter -lt $1 ]
+while [ $counter -lt $count_error_case ]
 do
     error_case=$(jq .error[$counter] result.json)
     echo "::error::$error_case"
