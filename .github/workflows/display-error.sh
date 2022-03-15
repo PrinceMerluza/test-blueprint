09f444396489291d78995cc5226c524d8e04a672
Prince Merluza <zero.bash@gmail.com>
2022-03-15 11:31:26 +0800

    Add shell script for displayin errors

---
+++ .github/workflows/display-error.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+count_failed_case=$(jq '.failed|length' result.json)
+count_error_case=$(jq '.error|length' result.json)
+
+counter=0
+while [ $counter -lt $1 ]
+do
+    failed_case=$(jq .failed[$counter] result.json)
+    echo "::error::$failed_case"
+    ((counter++))
+done
+
+counter=0
+while [ $counter -lt $1 ]
+do
+    error_case=$(jq .error[$counter] result.json)
+    echo "::error::$error_case"
+    ((counter++))
+done
