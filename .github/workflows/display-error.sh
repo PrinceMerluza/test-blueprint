f7f3fae479f92f1ccfcdafb1e4d4e0437ce20f6f
Prince Merluza <zero.bash@gmail.com>
2022-03-24 18:48:10 +0800

    Fix workflow script

--- .github/workflows/display-error.sh
+++ .github/workflows/display-error.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
 count_failed_case=$(jq '.failed|length' result.json)
-count_error_case=$(jq '.error|length' result.json)
 
 counter=0
 while [ $counter -lt $count_failed_case ]
 do
-    failed_case=$(jq .failed[$counter].description result.json)
+    failed_case=$(jq .failed[$counter].description linter-result.json)
     echo "::error::$failed_case"
     ((counter++))
 done
 
-counter=0
-while [ $counter -lt $count_error_case ]
-do
-    error_case=$(jq .error[$counter].description result.json)
-    echo "::error::$error_case"
-    ((counter++))
-done
-
-if [ $count_failed_case -gt 0 ] || [ $count_error_case -gt 0]
+if [ $count_failed_case -gt 0 ]
 then
     exit 1
 fi
