434c19ab535b4158f70d01553a39e4d306a09b02
Prince Merluza <zero.bash@gmail.com>
2022-03-24 19:06:23 +0800

    Try line highlighting

--- .github/workflows/display-error.sh
+++ .github/workflows/display-error.sh
@@ -6,7 +6,17 @@ counter=0
 while [ $counter -lt $count_failed_case ]
 do
     failed_case=$(jq .failed[$counter].description linter-result.json)
-    echo "::error::$failed_case"
+
+    line_counter=0
+    count_line_highlights = $(jq ".failed[$counter].fileHighlights|length" linter-result.json)
+    while [ $line_counter -lt $count_line_highlights ]
+    do
+        path=$(jq ".failed[$counter].fileHighlights[$line_counter].path" linter-result.json)
+        lineFrom=$(jq ".failed[$counter].fileHighlights[$line_counter].lineNumber" linter-result.json)
+        echo "::error file=$path,line=$lineFrom::$failed_case"
+        ((line_counter++))
+    done
+
     ((counter++))
 done
 
